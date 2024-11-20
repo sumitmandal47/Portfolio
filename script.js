0f5fef281a8954559f6e9ee57e86d3523b6c8cf9
sumit mandal <mandalsumit615@gmail.com>
2024-11-20 18:14:01 +0530

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let typed = new Typed(".text", {
+  strings: ["Frontend  Developer", "Web Developer", "Apps developer"],
+  typeSpeed: 90,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
